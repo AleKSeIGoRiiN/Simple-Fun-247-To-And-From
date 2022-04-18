a277663d986cd53a25edab57ab8f806ada6baff4
AleKSeIGoRiiN <80942002+AleKSeIGoRiiN@users.noreply.github.com>
2022-04-18 10:48:40 +0700

    Задание 1 - unit-тестирование

---
+++ UnitTest/CodewarsTaskTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToAndFrom;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CodewarsTaskTest
+    {
+        // unit-тесты
+
+        [TestMethod]
+        public void algorithmTest() // работа алгоритма
+        {
+            CodewarsTask Task = new CodewarsTask();
+            int arg1 = 10;
+            int arg2 = 15;
+            int arg3 = 6;
+            int expected = 14;
+
+            int result = Task.ToAndFrom(arg1, arg2, arg3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void timeTest()  // третий аргумент (время) не может быть меньше трёх
+        {
+            CodewarsTask Task = new CodewarsTask();
+            int arg1 = 125;
+            int arg2 = 345;
+            int arg3 = 2;
+            int expected = 0;
+
+            int result = Task.ToAndFrom(arg1, arg2, arg3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void equalityTest() // первый и второй аргумент не могут быть равны
+        {
+            CodewarsTask Task = new CodewarsTask();
+            int arg1 = 10;
+            int arg2 = 10;
+            int arg3 = 5;
+            int expected = 0;
+
+            int result = Task.ToAndFrom(arg1, arg2, arg3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void positiveArg1Test() // первый аргумент должен быть положительный
+        {
+            CodewarsTask Task = new CodewarsTask();
+            int arg1 = -1;
+            int arg2 = 10;
+            int arg3 = 8;
+            int expected = 0;
+
+            int result = Task.ToAndFrom(arg1, arg2, arg3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void positiveArg2Test() // второй аргумент должен быть положительный
+        {
+            CodewarsTask Task = new CodewarsTask();
+            int arg1 = 10;
+            int arg2 = -17;
+            int arg3 = 28;
+            int expected = 0;
+
+            int result = Task.ToAndFrom(arg1, arg2, arg3);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b7bcaba-a194-44e8-8711-1cb6746772cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
